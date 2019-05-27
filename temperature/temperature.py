c4f5e9a07c6540c42adcc11ef48db3e1a32f97a1
stressis <boucardstressis@gmail.com>
2019-05-27 13:03:32 +0200

    temperature

---
+++ temperature/temperature.py
@@ -0,0 +1,9 @@
+temperatures_C = [33,66,65,0,59,60,62,64,70,76,80,69,80,83,68,79,61,53,50,49,53,48,45,39]
+
+minimun = min(temperatures_C)
+print("le minimun est de ",minimun)
+maximum = max(temperatures_C)
+print("le maximum est de",maximum)
+somme = sum(temperatures_C)
+mean = somme/len(temperatures_C)
+print("la moyenne est de",mean)
